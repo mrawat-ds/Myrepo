7d22e0a5f552660e1942e9d3a7e99f2085a3bfae
mrawat-ds <61762220+mrawat-ds@users.noreply.github.com>
2021-09-04 20:20:27 -0700

    test commit

---
+++ Hello world.py
@@ -0,0 +1,10 @@
+import sys
+import pdb
+print("Hello World")
+
+print (2+2)
+print(3+4)
+pdb.set_trace()
+print(2+9)
+
+print(sys.version_info)
--- pandas intro.py
+++ pandas intro.py
@@ -1,3 +1,11 @@
 import numpy as np 
 import pandas as pd 
-df = pd.read_csv('/download/data/survey_results_public.csv')
+df = pd.read_csv('/Users/manojrawat/downloads/data/survey_results_public.csv')
+print(df.shape)
+print(df.info)
+df.iloc[0]
+df.iloc[[0,3]]
+
+df.iloc[[0,3],2]
+df.loc[0]
+df.loc[[0,1],'Respondent']
